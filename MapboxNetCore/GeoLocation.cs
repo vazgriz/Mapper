83bcac5f9d4b76a1d18371cb5afb70c536d09f2e
Ryan Vazquez <contact@ryan-vazquez.dev>
2022-04-10 15:31:08 -0500

    Move converter definition

--- MapboxNetCore/GeoLocation.cs
+++ MapboxNetCore/GeoLocation.cs
@@ -59,18 +59,4 @@ namespace MapboxNetCore
             return value == null ? null : value.ToString();
         }
     }
-
-    public class GeoLocationDisplayConverter : IValueConverter {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-            var location = (GeoLocation)value;
-
-            if (location == null) return "ERROR";
-
-            return string.Format("{0:0.000}, {1:0.000}", location.Latitude, location.Longitude);
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
-            throw new NotImplementedException();
-        }
-    }
 }
---
+++ Mapper/Converters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using MapboxNetCore;
+
+namespace Mapper {
+    public class GeoLocationDisplayConverter : System.Windows.Data.IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            var location = (GeoLocation)value;
+
+            if (location == null) return "ERROR";
+
+            return string.Format("{0:0.000}, {1:0.000}", location.Latitude, location.Longitude);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
