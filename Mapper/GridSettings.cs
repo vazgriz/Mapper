5de2260d1984628e50945763fe617652487330f6
vazgriz <contact@ryan-vazquez.dev>
2026-03-25 15:13:40 -0500

    Add defaults for map file loading

--- Mapper/GridSettings.cs
+++ Mapper/GridSettings.cs
@@ -1,24 +1,39 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using Newtonsoft.Json.Linq;
 
 namespace Mapper {
     public class GridSettings : INotifyPropertyChanged {
-        double coordX;
-        double coordY;
-        double gridSize = 20;
-        int outputSize = 1024;
-        int tileSize = 1024;
-        int tileCount = 1;
-        float heightMin;
-        float heightMax;
-        float heightDifference;
-        bool flipOutput;
-        bool downloadSatelliteImages;
-        bool applyWaterOffset;
-        float waterOffset;
-        bool forceZipExport;
+        const double coordXDefault = 0;
+        const double coordYDefault = 0;
+        const double gridSizeDefault = 20;
+        const int outputSizeDefault = 1024;
+        const int tileSizeDefault = 1024;
+        const int tileCountDefault = 1;
+        const float heightMinDefault = 0;
+        const float heightMaxDefault = 0;
+        const float heightDifferenceDefault = 0;
+        const bool flipOutputDefault = true;
+        const bool downloadSatelliteImagesDefault = false;
+        const bool applyWaterOffsetDefault = false;
+        const float waterOffsetDefault = 0;
+        const bool forceZipExportDefault = false;
+
+        double coordX = coordXDefault;
+        double coordY = coordYDefault;
+        double gridSize = gridSizeDefault;
+        int outputSize = outputSizeDefault;
+        int tileSize = tileSizeDefault;
+        int tileCount = tileCountDefault;
+        float heightMin = heightMinDefault;
+        float heightMax = heightMaxDefault;
+        float heightDifference = heightDifferenceDefault;
+        bool flipOutput = flipOutputDefault;
+        bool downloadSatelliteImages = downloadSatelliteImagesDefault;
+        bool applyWaterOffset = applyWaterOffsetDefault;
+        float waterOffset = waterOffsetDefault;
+        bool forceZipExport = forceZipExportDefault;
 
         public Version Version { get; set; }    //not used by XAML
 
@@ -180,6 +195,38 @@ namespace Mapper {
             return GridSize > 0 && OutputSize >= 256 && TileCount > 0 && customHeightValid;
         }
 
+        int GetOrDefault(JObject obj, string name, int defaultValue) {
+            if (obj.TryGetValue(name, out var value)) {
+                return (int)value;
+            } else {
+                return defaultValue;
+            }
+        }
+
+        double GetOrDefault(JObject obj, string name, double defaultValue) {
+            if (obj.TryGetValue(name, out var value)) {
+                return (double)value;
+            } else {
+                return defaultValue;
+            }
+        }
+
+        float GetOrDefault(JObject obj, string name, float defaultValue) {
+            if (obj.TryGetValue(name, out var value)) {
+                return (float)value;
+            } else {
+                return defaultValue;
+            }
+        }
+
+        bool GetOrDefault(JObject obj, string name, bool defaultValue) {
+            if (obj.TryGetValue(name, out var value)) {
+                return (bool)value;
+            } else {
+                return defaultValue;
+            }
+        }
+
         public void CopyFrom(JObject other) {
             var versionString = (string)other[nameof(Version)];
             var otherVersion = Version.Parse(versionString);
@@ -188,20 +235,20 @@ namespace Mapper {
                 UpdateSettings(other);
             }
 
-            CoordinateX = (double)other[nameof(CoordinateX)];
-            CoordinateY = (double)other[nameof(CoordinateY)];
-            GridSize = (double)other[nameof(GridSize)];
-            OutputSize = (int)other[nameof(OutputSize)];
-            TileSize = (int)other[nameof(TileSize)];
-            TileCount = (int)other[nameof(TileCount)];
-            HeightMin = (float)other[nameof(HeightMin)];
-            HeightMax = (float)other[nameof(HeightMax)];
-            HeightDifference = (float)other[nameof(HeightDifference)];
-            FlipOutput = (bool)other[nameof(FlipOutput)];
-            DownloadSatelliteImages = (bool)other[nameof(DownloadSatelliteImages)];
-            ApplyWaterOffset = (bool)other[nameof(ApplyWaterOffset)];
-            WaterOffset = (float)other[nameof(WaterOffset)];
-            ForceZipExport = (bool)other[nameof(ForceZipExport)];
+            CoordinateX = GetOrDefault(other, nameof(CoordinateX), coordXDefault);
+            CoordinateY = GetOrDefault(other, nameof(CoordinateY), coordYDefault);
+            GridSize = GetOrDefault(other, nameof(GridSize), gridSizeDefault);
+            OutputSize = GetOrDefault(other, nameof(OutputSize), outputSizeDefault);
+            TileSize = GetOrDefault(other, nameof(TileSize), tileSizeDefault);
+            TileCount = GetOrDefault(other, nameof(TileCount), tileCountDefault);
+            HeightMin = GetOrDefault(other, nameof(HeightMin), heightMinDefault);
+            HeightMax = GetOrDefault(other, nameof(HeightMax), heightMaxDefault);
+            HeightDifference = GetOrDefault(other, nameof(HeightDifference), heightDifferenceDefault);
+            FlipOutput = GetOrDefault(other, nameof(FlipOutput), flipOutputDefault);
+            DownloadSatelliteImages = GetOrDefault(other, nameof(DownloadSatelliteImages), downloadSatelliteImagesDefault);
+            ApplyWaterOffset = GetOrDefault(other, nameof(ApplyWaterOffset), applyWaterOffsetDefault);
+            WaterOffset = GetOrDefault(other, nameof(WaterOffset), waterOffsetDefault);
+            ForceZipExport = GetOrDefault(other, nameof(ForceZipExport), forceZipExportDefault);
         }
 
         void UpdateSettings(JObject other) {
